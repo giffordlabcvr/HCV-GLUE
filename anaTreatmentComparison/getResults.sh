92ceda44b02983e9aeccb29a3cea2f7803ede8af
Josh Singer <josh.singer@glasgow.ac.uk>
2016-03-24 16:23:56 +0000

    major tidy up

--- anaTreatmentComparison/getResults.sh
+++
@@ -1,7 +0,0 @@
-gluetools.sh -p cmd-result-format:csv -E -i project HCV list sequence -w "source.name = 'treatmentComparison'" sequenceID genotype subtype RESPONDED POST_TREATMENT > /tmp/sequences.txt
-
-gluetools.sh -p cmd-result-format:csv -E -i project HCV module mutationFrequencies alignment scan variation-category AL_MASTER -r -w "sequence.source.name = 'treatmentComparison'" H77_AF009606 NS3 resistance_associated > /tmp/NS3_RAVs.txt
-
-gluetools.sh -p cmd-result-format:csv -E -i project HCV module mutationFrequencies alignment scan variation-category AL_MASTER -r -w "sequence.source.name = 'treatmentComparison'" H77_AF009606 NS5A resistance_associated > /tmp/NS5A_RAVs.txt
-
-gluetools.sh -p cmd-result-format:csv -E -i project HCV module mutationFrequencies alignment scan variation-category AL_MASTER -r -w "sequence.source.name = 'treatmentComparison'" H77_AF009606 NS5B resistance_associated > /tmp/NS5B_RAVs.txt
