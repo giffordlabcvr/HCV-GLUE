d21bf65ded9908c5d3a7c71e2e3c06a05b7b9068
Josh Singer <josh.singer@glasgow.ac.uk>
2016-11-03 16:08:06 +0000

    added paging to the RAVs page.

--- WebContent/controllers/hcvRavs.js
+++ WebContent/controllers/hcvRavs.js
@@ -1,35 +1,56 @@
 hcvApp.controller('hcvRavsCtrl', 
-		[ '$scope', '$route', '$routeParams', 'glueWS', 'dialogs', 
-		  function($scope, $route, $routeParams, glueWS, dialogs) {
+		[ '$scope', '$route', '$routeParams', 'glueWS', 'dialogs', 'pagingContext', 
+		  function($scope, $route, $routeParams, glueWS, dialogs, pagingContext) {
 
 			addUtilsToScope($scope);
 
 			$scope.ravs = [];
 
+			$scope.pagingContext = null;
+			$scope.whereClause = "is_resistance_associated_variant = 'true'"
+			$scope.sortProperties = "featureLoc.feature.name,rav_first_codon,rav_substitutions";
+			$scope.fieldNames = [
+					                "featureLoc.feature.name",
+					                "rav_substitutions",
+					                "rav_first_codon",
+					                "featureLoc.referenceSequence.name",
+					                "name"
+					            ];
+
+			
 			glueWS.runGlueCommand("", {
-			    "list":{
-			        "variation":{
-			            "whereClause":"is_resistance_associated_variant = 'true'",
-			            "fieldName":[
-			                "featureLoc.feature.name",
-			                "rav_substitutions",
-			                "rav_first_codon",
-			                "featureLoc.referenceSequence.name",
-			                "name"
-			            ]
-			        }
-			    }
-			})
-			.success(function(data, status, headers, config) {
-				$scope.ravs = tableResultAsObjectList(data);
-				// chained sort: least significant field first
-				$scope.ravs = _($scope.ravs).chain()
-				  .sortBy('rav_substitutions')
-				  .sortBy('rav_first_codon')
-				  .sortBy('featureLoc.feature.name')
-				  .value();
-				console.info('$scope.ravs', $scope.ravs);
-			})
-			.error(glueWS.raiseErrorDialog(dialogs, "retrieving resistance associated variants"));
+				"count":{
+					"variation":{
+						"whereClause":$scope.whereClause
+					}
+				}
+			}
+			)
+		    .success(function(data, status, headers, config) {
+				console.info('count variations raw result', data);
+				$scope.pagingContext = pagingContext.createPagingContext(data.countResult.count, 10, $scope.updatePage);
+		    })
+		    .error(glueWS.raiseErrorDialog(dialogs, "counting variations"));
+
+			
+			$scope.updatePage = function(pContext) {
+				console.log("updatePage", pContext);
+				var cmdParams = {
+			            "whereClause":$scope.whereClause,
+			            "sortProperties":$scope.sortProperties,
+			            "fieldName":$scope.fieldNames
+				};
+				cmdParams.pageSize = pContext.itemsPerPage;
+				cmdParams.fetchLimit = pContext.itemsPerPage;
+				cmdParams.fetchOffset = pContext.firstItemIndex - 1;
+				glueWS.runGlueCommand("", {
+			    	"list": { "variation": cmdParams } 
+				})
+				.success(function(data, status, headers, config) {
+					$scope.ravs = tableResultAsObjectList(data);
+					console.info('$scope.ravs', $scope.ravs);
+				})
+				.error(glueWS.raiseErrorDialog(dialogs, "retrieving resistance associated substitutions"));
+			}
 
 		}]);
