b56e056bdb09a201d44e45f019886d1ca682e220
Josh Singer <josh.singer@glasgow.ac.uk>
2017-01-13 10:52:54 +0000

    update to gb lab construct to make sure it is false for ncbi-curated.

--- WebContent/controllers/hcvSequences.js
+++ WebContent/controllers/hcvSequences.js
@@ -14,6 +14,7 @@ projectBrowser.controller('hcvSequencesCtrl',
 					["source.name",
 					 "sequenceID",
 					 "gb_create_date",
+                     "gb_update_date",
 					 "gb_length",
 					 "gb_isolate",
                      "gb_country_iso",
@@ -23,7 +24,8 @@ projectBrowser.controller('hcvSequencesCtrl',
 			
 			$scope.pagingContext.setSortableProperties([
 	            { property:"sequenceID", displayName: "NCBI Nucleotide ID" },
-	            { property:"gb_create_date", displayName: "Creation Date" },
+	            { property:"gb_create_date", displayName: "NCBI Entry Creation Date" },
+	            { property:"gb_update_date", displayName: "NCBI Last Update Date" },
   	            { property:"gb_country_iso", displayName: "Country of Origin" },
 	            { property:"gb_collection_year", displayName: "Collection Year" },
 	            { property:"gb_isolate", displayName: "Isolate ID" },
@@ -38,11 +40,14 @@ projectBrowser.controller('hcvSequencesCtrl',
 			$scope.pagingContext.setFilterProperties([
          		{ property:"sequenceID", displayName: "NCBI Nucleotide ID", filterHints: {type: "String"} },
         		{ property:"gb_length", displayName: "Sequence Length", filterHints: {type: "Integer"} },
-        		{ property:"gb_create_date", displayName: "Creation Date", filterHints: {type: "Date"} },
+        		{ property:"gb_create_date", displayName: "NCBI Entry Creation Date", filterHints: {type: "Date"} },
+        		{ property:"gb_update_date", displayName: "NCBI Last Update Date", filterHints: {type: "Date"} },
   	            { property:"gb_country_short", altProperties:["gb_country_iso"], displayName: "Country of Origin", filterHints: {type: "String"} },
 	            { property:"gb_collection_year", displayName: "Collection Year", filterHints: {type: "Integer"} },
 	            { property:"gb_isolate", displayName: "Isolate ID", filterHints: {type: "String"} },
-	            { property:"gb_pubmed_id", displayName: "PubMed ID", filterHints: {type: "String"} }
+	            { property:"gb_pubmed_id", displayName: "PubMed ID", filterHints: {type: "String"} },
+	            { property:"gb_lab_construct", displayName: "Lab Construct?", filterHints: {type: "Boolean"} },
+	            { property:"gb_recombinant", displayName: "Recombinant?", filterHints: {type: "Boolean"} }
 			]);
 			                          			
 			$scope.pagingContext.setDefaultFilterElems([]);
