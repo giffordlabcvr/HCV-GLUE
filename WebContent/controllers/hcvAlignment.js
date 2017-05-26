0df69cbf11cdae89426c28ec4be43d12e2cd9dfe
Josh Singer <josh.singer@glasgow.ac.uk>
2017-05-26 10:01:53 +0100

    more stuff relating to Global Regions.

--- WebContent/controllers/hcvAlignment.js
+++ WebContent/controllers/hcvAlignment.js
@@ -1,6 +1,6 @@
 hcvApp.controller('hcvAlignmentCtrl', 
-		[ '$scope', '$routeParams', '$controller', 'glueWS', 'glueWebToolConfig', 'dialogs', 'pagingContext',
-		  function($scope, $routeParams, $controller, glueWS, glueWebToolConfig, dialogs, pagingContext) {
+		[ '$scope', '$routeParams', '$controller', 'glueWS', 'glueWebToolConfig', 'dialogs', 'pagingContext', 'filterUtils',
+		  function($scope, $routeParams, $controller, glueWS, glueWebToolConfig, dialogs, pagingContext, filterUtils) {
 			
 			addUtilsToScope($scope);
 
@@ -27,6 +27,54 @@ hcvApp.controller('hcvAlignmentCtrl',
                      "sequence.gb_isolate"
                  ]);
 
+			glueWS.runGlueCommand("", {
+			    "multi-render":{
+			        "tableName":"who_region",
+			        "allObjects":"true",
+			        "rendererModuleName":"whoRegionTreeRenderer"
+			    }
+			})
+			.success(function(data, status, headers, config) {
+				var multiRenderResult = data.multiRenderResult;
+				console.info('who region multi-render result', data.multiRenderResult);
+				$scope.globalRegionFixedValueSet = [];
+				for(var i = 0; i < multiRenderResult.resultDocument.length; i++) {
+					var whoRegion = multiRenderResult.resultDocument[i].whoRegion;
+					$scope.globalRegionFixedValueSet.push({
+						property:"sequence.who_country.who_region",
+			   			  value:whoRegion.id,
+			   			  indent:0,
+			   			  displayName:whoRegion.displayName
+					});
+					if(whoRegion.whoSubRegion != null) {
+						for(var j = 0; j < whoRegion.whoSubRegion.length; j++) {
+							var whoSubRegion = whoRegion.whoSubRegion[j];
+							$scope.globalRegionFixedValueSet.push({
+								property:"sequence.who_country.who_sub_region",
+					   			  value:whoSubRegion.id,
+					   			  indent:1,
+					   			  displayName:whoSubRegion.displayName
+							});
+							if(whoSubRegion.whoIntermediateRegion != null) {
+								for(var k = 0; k < whoSubRegion.whoIntermediateRegion.length; k++) {
+									var whoIntermediateRegion = whoSubRegion.whoIntermediateRegion[k];
+									$scope.globalRegionFixedValueSet.push({
+										property:"sequence.who_country.who_intermediate_region",
+							   			  value:whoIntermediateRegion.id,
+							   			  indent:2,
+							   			  displayName:whoIntermediateRegion.displayName
+									});
+								}
+							}
+						}
+					}
+				}
+				console.info('$scope.globalRegionFixedValueSet', $scope.globalRegionFixedValueSet);
+			})
+			.error(glueWS.raiseErrorDialog(dialogs, "retrieving WHO region tree"));
+
+			$scope.globalRegionFixedValueSet = [];
+			
 			$scope.pagingContext.setDefaultSortOrder([
 			    { property: "sequence.sequenceID", displayName: "NCBI Nucleotide ID", order: "+" }
 			]);
@@ -59,7 +107,8 @@ hcvApp.controller('hcvAlignmentCtrl',
                 			  minCoveragePct: 90.0
                 		  };
                 	  },
-                	  generatePredicateFromCustom: function(custom) {
+                	  generatePredicateFromCustom: function(filterElem) {
+                		  var custom = filterElem.custom;
                 		  var cayennePredicate = 
                   		  	"fLocNotes.featureLoc.referenceSequence.name = '"+$scope.referenceName+"' and "+
                 		  	"fLocNotes.featureLoc.feature.name = '"+custom.feature.featureName+"' and "+
@@ -74,6 +123,37 @@ hcvApp.controller('hcvAlignmentCtrl',
                 		  return($scope.nonInformationalFeatureList);
                 	  }
                 	}
+                },
+                // note property here is a dummy value.
+                { property:"whoCountry", nullProperty:"sequence.who_country", displayName: "Global Region", filterHints: 
+	            	{ type: "StringFromFixedValueSet",
+                	  generateCustomDefault: function() {
+	            		  return {
+	            			  fixedValue: $scope.globalRegionFixedValueSet[0]
+	            		  };
+	            	  },
+                	  generatePredicateFromCustom: function(filterElem) {
+                		  var custom = filterElem.custom;
+                		  var fakeFilterElem = {
+                		    property:custom.fixedValue.property,
+                		    nullProperty:filterElem.nullProperty,
+                			type: filterElem.type,
+                			predicate: {
+                				operator: filterElem.predicate.operator,
+                				operand: [custom.fixedValue.value]
+                			}
+                		  };
+                		  var cayennePredicate = filterUtils.filterElemToCayennePredicate(fakeFilterElem);
+                		  // we want notmatches here to allow sequences with countries that have a null region/subregion/intregion.
+                		  if(filterElem.predicate.operator == 'notmatches') {
+                			  cayennePredicate = "( ( "+custom.fixedValue.property + " = null ) or ( " + cayennePredicate + " ) )";
+                		  }
+                		  return cayennePredicate;
+                  	  },
+	            	  generateFixedValueSet: function() {
+	            		  return $scope.globalRegionFixedValueSet;
+	            	  }
+	            	}
                 },
           		{ property:"sequence.gb_create_date", displayName: "NCBI Entry Creation Date", filterHints: {type: "Date"} },
           		{ property:"sequence.gb_update_date", displayName: "NCBI Last Update Date", filterHints: {type: "Date"} },
