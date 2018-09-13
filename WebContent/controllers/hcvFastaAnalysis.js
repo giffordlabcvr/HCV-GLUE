f23140b2249211b47b831c2682bcd0463cdbc008
Josh Singer <josh.singer@glasgow.ac.uk>
2018-09-13 10:21:23 +0100

    Web genotyping working using the new methods. Basic summary report with genotyping results and drug resistance.

--- WebContent/controllers/hcvFastaAnalysis.js
+++ WebContent/controllers/hcvFastaAnalysis.js
@@ -19,7 +19,6 @@ hcvApp.controller('hcvFastaAnalysisCtrl',
 				}
 			});
 			
-
 			
 		    // CALLBACKS
 		    $scope.uploader.onBeforeUploadItem = function(item) {
@@ -43,37 +42,107 @@ hcvApp.controller('hcvFastaAnalysisCtrl',
 				$scope.analytics.eventTrack("hcvFastaAnalysisResult", 
 						{  category: 'hcvFastaAnalysis', 
 							label: 'fileName:'+fileItem.file.name+',fileSize:'+fileItem.file.size });
-				console.log(response);
+				fileItem.response = response;
+				console.log("hcvFastaAnalysis.response", response);
 		    };
 		    $scope.uploader.onErrorItem = function(fileItem, response, status, headers) {
 		        console.info('onErrorItem', fileItem, response, status, headers);
 		        var errorFn = glueWS.raiseErrorDialog(dialogs, "processing sequence file \""+fileItem.file.name+"\"");
 		        errorFn(response, status, headers, {});
 		    };
-		    $scope.uploader.onWhenAddingFileFailed = function(item, filter, options) {
-		        console.info('onWhenAddingFileFailed', item, filter, options);
+
+			$scope.removeAll = function() {
+				$scope.uploader.clearQueue();
+				$scope.fileItemUnderAnalysis = null;
+			}
+
+			$scope.removeItem = function(item) {
+				if($scope.fileItemUnderAnalysis == item) {
+					$scope.fileItemUnderAnalysis = null;
+				}
+				item.remove();
+			}
+		    
+		    $scope.showAnalysisResults = function(item) {
+		    	$scope.fileItemUnderAnalysis = item;
+				$scope.summary = true;
+				$scope.genomeVisualisation = false;
 		    };
-		    $scope.uploader.onAfterAddingFile = function(fileItem) {
-		        console.info('onAfterAddingFile', fileItem);
-		    };
-		    $scope.uploader.onAfterAddingAll = function(addedFileItems) {
-		        console.info('onAfterAddingAll', addedFileItems);
-		    };
-		    $scope.uploader.onProgressItem = function(fileItem, progress) {
-		        console.info('onProgressItem', fileItem, progress);
-		    };
-		    $scope.uploader.onProgressAll = function(progress) {
-		        console.info('onProgressAll', progress);
-		    };
-		    $scope.uploader.onCancelItem = function(fileItem, response, status, headers) {
-		        console.info('onCancelItem', fileItem, response, status, headers);
-		    };
-		    $scope.uploader.onCompleteItem = function(fileItem, response, status, headers) {
-		        console.info('onCompleteItem', fileItem, response, status, headers);
-		    };
-		    $scope.uploader.onCompleteAll = function() {
-		        console.info('onCompleteAll');
+			
+		    $scope.getGenotype = function(sequenceResult) {
+		    	var genotypeCladeResult = _.find(sequenceResult.genotypingResult.queryCladeCategoryResult, function(qccr) {return qccr.categoryName == 'genotype'});
+		    	if(genotypeCladeResult == null) {
+		    		return "-";
+		    	}
+		    	return genotypeCladeResult.shortRenderedName;
 		    };
 
-			
+		    $scope.getSubtype = function(sequenceResult) {
+		    	var subtypeCladeResult = _.find(sequenceResult.genotypingResult.queryCladeCategoryResult, function(qccr) {return qccr.categoryName == 'subtype'});
+		    	if(subtypeCladeResult == null) {
+		    		return "-";
+		    	}
+		    	return subtypeCladeResult.shortRenderedName;
+		    };
+
+		    $scope.getClosestReferenceSequence = function(sequenceResult) {
+		    	var genotypeCladeResult = _.find(sequenceResult.genotypingResult.queryCladeCategoryResult, function(qccr) {return qccr.categoryName == 'genotype'});
+		    	if(genotypeCladeResult == null) {
+		    		return "-";
+		    	}
+		    	return genotypeCladeResult.closestTargetSequenceID;
+		    };
+
+		    $scope.getResistanceLevel = function(sequenceResult, drugId) {
+		    	if(sequenceResult.drugScores != null) { 
+		    		for(var i = 0; i < sequenceResult.drugScores.length; i++) {
+		    			var categoryScores = sequenceResult.drugScores[i];
+		    			for(var j = 0; j < categoryScores.drugAssessments.length; j++) {
+		    				var drugAssessment = categoryScores.drugAssessments[j];
+		    				if(drugAssessment.drug.id == drugId) {
+		    					return drugAssessment.drugScore;
+		    				}
+		    			}
+		    		}
+		    	}
+		    	return "none";
+		    };
+
+		    $scope.getResistanceLevelText = function(sequenceResult, drugId) {
+		    	if(sequenceResult.drugScores != null) { 
+		    		for(var i = 0; i < sequenceResult.drugScores.length; i++) {
+		    			var categoryScores = sequenceResult.drugScores[i];
+		    			for(var j = 0; j < categoryScores.drugAssessments.length; j++) {
+		    				var drugAssessment = categoryScores.drugAssessments[j];
+		    				if(drugAssessment.drug.id == drugId) {
+		    					return drugAssessment.drugScoreDisplayShort;
+		    				}
+		    			}
+		    		}
+		    	}
+		    	return "-";
+		    };
+
+			$scope.$watch('summary', function(newObj, oldObj) {
+				if(!$scope.updating) {
+					$scope.updating = true;
+					if(newObj) {
+						$scope.genomeVisualisation = false;
+					}
+					$scope.updating = false;
+				}
+			}, false);
+
+			$scope.$watch('genomeVisualisation', function(newObj, oldObj) {
+				if(!$scope.updating) {
+					$scope.updating = true;
+					if(newObj) {
+						$scope.summary = false;
+					}
+					$scope.updating = false;
+				}
+			}, false);
+
+		    
+		    
 		}]);
