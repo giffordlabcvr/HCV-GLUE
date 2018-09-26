659b08b11a09357e00fbe178730b00cf5ec477ae
Josh Singer <josh.singer@glasgow.ac.uk>
2018-09-26 15:36:28 +0100

    Various further fleshing out of the browsability.

--- WebContent/controllers/hcvAlignment.js
+++ WebContent/controllers/hcvAlignment.js
@@ -41,7 +41,6 @@ hcvApp.controller('hcvAlignmentCtrl',
 			
 			$scope.pagingContext.setDefaultSortOrder([
   	            { property:"sequence.gb_create_date", displayName: "NCBI Entry Creation Date", order: "-" }
-			    // { property: "sequence.sequenceID", displayName: "NCBI Nucleotide ID", order: "+" }
 			]);
 
 			
@@ -78,14 +77,14 @@ hcvApp.controller('hcvAlignmentCtrl',
   			]);
   			                          			
 
-			$scope.rasNoteWhereClause = "variation.phdr_ras != null and alignment.name = '"+$scope.almtName+"'";
+			$scope.freqNoteWhereClause = "variation.phdr_ras.phdr_alignment_ras.id != null and alignment.name = '"+$scope.almtName+"'";
 
-			$scope.updateRasPage = function(pContext) {
-				console.log("updateRasPage", pContext);
+			$scope.updateFreqNotePage = function(pContext) {
+				console.log("updateFreqNotePage", pContext);
 				
 				var cmdParams = {
 						"tableName": "var_almt_note",
-			            "whereClause": $scope.rasNoteWhereClause,
+			            "whereClause": $scope.freqNoteWhereClause,
 			            "allObjects": false,
 			            "rendererModuleName": "phdrRasFrequencyRenderer"
 				};
@@ -95,16 +94,16 @@ hcvApp.controller('hcvAlignmentCtrl',
 				})
 			    .success(function(data, status, headers, config) {
 			    	$scope.rasNoteList = data.multiRenderResult.resultDocument;
-					console.info('listing RAS frequency notes result as object list', $scope.rasNoteList);
+					console.info('listing RAP frequency notes result as object list', $scope.rasNoteList);
 			    })
-			    .error(glueWS.raiseErrorDialog(dialogs, "listing RAS frequency notes"));
+			    .error(glueWS.raiseErrorDialog(dialogs, "listing RAP frequency notes"));
 			}
 			
-			$scope.updateRasCount = function(pContext) {
-				console.log("updateRasCount", pContext);
+			$scope.updateFreqNoteCount = function(pContext) {
+				console.log("updateFreqNoteCount", pContext);
 				
 				var cmdParams = {
-			            "whereClause": $scope.rasNoteWhereClause
+			            "whereClause": $scope.freqNoteWhereClause
 				};
 				pContext.extendCountCmdParams(cmdParams);
 				glueWS.runGlueCommand("", {
@@ -114,37 +113,114 @@ hcvApp.controller('hcvAlignmentCtrl',
 				})
 			    .success(function(data, status, headers, config) {
 					console.info('count RAS frequency notes raw result', data);
-					$scope.rasPagingContext.setTotalItems(data.countResult.count);
-					$scope.rasPagingContext.firstPage();
+					$scope.freqNotePagingContext.setTotalItems(data.countResult.count);
+					$scope.freqNotePagingContext.firstPage();
 			    })
-			    .error(glueWS.raiseErrorDialog(dialogs, "counting RAS frequency notes"));
+			    .error(glueWS.raiseErrorDialog(dialogs, "counting RAP frequency notes"));
 			}
 			
-			$scope.rasPagingContext = pagingContext.createPagingContext($scope.updateRasCount, $scope.updateRasPage);
+			$scope.freqNotePagingContext = pagingContext.createPagingContext($scope.updateFreqNoteCount, $scope.updateFreqNotePage);
 
-			$scope.rasPagingContext.setDefaultSortOrder([
+			$scope.freqNotePagingContext.setDefaultSortOrder([
  	            { property:"variation.phdr_ras.gene", displayName: "Virus protein", order: "+"  },
 	            { property:"variation.phdr_ras.sort_key", displayName: "Polymorphic locations", order: "+" }
 			]);
 
-			$scope.rasPagingContext.setSortableProperties([
+			$scope.freqNotePagingContext.setSortableProperties([
    	            { property:"variation.phdr_ras.gene", displayName: "Virus protein" },
 	            { property:"variation.phdr_ras.sort_key", displayName: "Polymorphic locations" },
 	            { property:"ncbi_curated_frequency", displayName: "Frequency" }
             ]);
 
 			
-			$scope.rasPagingContext.setFilterProperties([
+			$scope.freqNotePagingContext.setFilterProperties([
 	     		{ property: "variation.phdr_ras.gene", displayName: "Virus protein", filterHints: {type: "String"} },
 	            { property: "variation.phdr_ras.phdr_alignment_ras.alignment.displayName", altProperties:["variation.phdr_ras.phdr_alignment_ras.alignment.name"], displayName: "Resisted genotype / subtype", filterHints: {type: "String"} },
 	            { property: "variation.phdr_ras.phdr_alignment_ras.phdr_alignment_ras_drug.phdr_drug.id", displayName: "Drug", altProperties:["variation.phdr_ras.phdr_alignment_ras.phdr_alignment_ras_drug.phdr_drug.abbreviation", "variation.phdr_ras.phdr_alignment_ras.phdr_alignment_ras_drug.phdr_drug.research_code"], filterHints: {type: "String"} },
 	    		{ property: "ncbi_curated_frequency", displayName: "Frequency (percentage)", filterHints: {type: "Double"} }
 			]);
 			                          			
-			$scope.rasPagingContext.setDefaultFilterElems([]);
+			$scope.freqNotePagingContext.setDefaultFilterElems([]);
 
 			
-			$scope.rasPagingContext.countChanged();
+			$scope.freqNotePagingContext.countChanged();
 
+			$scope.rapWhereClause = "phdr_alignment_ras.alignment.name = '"+$scope.almtName+"' or phdr_alignment_ras.alignment.parent.name = '"+$scope.almtName+"' or phdr_alignment_ras.alignment.parent.parent.name = '"+$scope.almtName+"'";
+
+			$scope.updateRapPage = function(pContext) {
+				console.log("updateRapPage", pContext);
+				
+				var cmdParams = {
+						"tableName": "phdr_alignment_ras_drug",
+			            "whereClause": $scope.rapWhereClause,
+			            "allObjects": false,
+			            "rendererModuleName": "phdrAlignmentRasDrugRenderer"
+				};
+				pContext.extendListCmdParams(cmdParams);
+				glueWS.runGlueCommand("", {
+				    "multi-render": cmdParams
+				})
+			    .success(function(data, status, headers, config) {
+			    	$scope.phdrAlignmentRasDrugs = data.multiRenderResult.resultDocument;
+					console.info('phdr_alignment_ras_drug objects', $scope.phdrAlignmentRasDrugs);
+			    })
+			    .error(glueWS.raiseErrorDialog(dialogs, "listing RAPs"));
+			}
+			
+			$scope.updateRapCount = function(pContext) {
+				console.log("updateRapCount", pContext);
+				
+				var cmdParams = {
+						"tableName": "phdr_alignment_ras_drug",
+			            "whereClause": $scope.rapWhereClause
+				};
+				pContext.extendCountCmdParams(cmdParams);
+				glueWS.runGlueCommand("", {
+			    	"count": { 
+				        "custom-table-row":cmdParams
+			    	} 
+				})
+			    .success(function(data, status, headers, config) {
+					console.info('count phdr_alignment_ras_drug objects raw result', data);
+					$scope.rapPagingContext.setTotalItems(data.countResult.count);
+					$scope.rapPagingContext.firstPage();
+			    })
+			    .error(glueWS.raiseErrorDialog(dialogs, "counting phdr_alignment_ras_drug objects"));
+			}
+			
+			$scope.rapPagingContext = pagingContext.createPagingContext($scope.updateRapCount, $scope.updateRapPage);
+
+			$scope.rapPagingContext.setDefaultSortOrder([
+   	            { property:"numeric_resistance_category", displayName: "Resistance category", order: "+" },
+ 	            { property:"phdr_alignment_ras.phdr_ras.gene", displayName: "Virus protein", order: "+" },
+  	            { property:"phdr_alignment_ras.phdr_ras.sort_key", displayName: "Polymorphic locations", order: "+" }
+			]);
+
+			$scope.rapPagingContext.setSortableProperties([
+ 	            { property:"phdr_alignment_ras.phdr_ras.gene", displayName: "Virus protein" },
+  	            { property:"phdr_alignment_ras.phdr_ras.sort_key", displayName: "Polymorphic locations" },
+	            { property:"numeric_resistance_category", displayName: "Resistance category"},
+	            { property:"in_vitro_max_ec50_midpoint", displayName: "EC50 log fold change"}
+            ]);
+
+			
+			$scope.rapPagingContext.setFilterProperties([
+ 	            { property:"phdr_alignment_ras.phdr_ras.gene", displayName: "Virus protein", filterHints: {type: "String"} },
+	            { property:"phdr_alignment_ras.alignment.displayName", altProperties:["alignment.name"], displayName: "Genotype / subtype", filterHints: {type: "String"} },
+	            { property:"phdr_drug.id", displayName: "Drug", altProperties:["phdr_drug.abbreviation", "phdr_drug.research_code"], filterHints: {type: "String"} },
+  	            { property:"any_in_vitro_evidence", displayName: "In vitro evidence?", filterHints: {type: "Boolean"} }, 
+  	            { property:"in_vitro_max_ec50_midpoint", displayName: "EC50 log fold change", filterHints: {type: "Double"} }, 
+  	            { property:"any_in_vivo_evidencee", displayName: "In vivo evidence?", filterHints: {type: "Boolean"} }, 
+  	            { property:"in_vivo_baseline", displayName: "Found at baseline?", filterHints: {type: "Boolean"} }, 
+  	            { property:"in_vivo_treatment_emergent", displayName: "Treatment emergent?", filterHints: {type: "Boolean"} }
+			]);
+			                          			
+			$scope.rapPagingContext.setDefaultFilterElems([]);
+
+			
+			$scope.rapPagingContext.countChanged();
+
+			
+			
 			
 		}]);
--- WebContent/controllers/hcvRap.js
+++ WebContent/controllers/hcvRap.js
@@ -1,11 +1,14 @@
 hcvApp.controller('hcvRapCtrl', 
-		[ '$scope', '$route', '$routeParams', 'glueWS', 'dialogs', 
-		  function($scope, $route, $routeParams, glueWS, dialogs) {
+		[ '$scope', '$route', '$routeParams', 'glueWS', 'dialogs', 'pagingContext',
+		  function($scope, $route, $routeParams, glueWS, dialogs, pagingContext) {
 
 			addUtilsToScope($scope);
 
 			$scope.rap = null;
 			$scope.rapId = $routeParams.id;
+			
+			$scope.displaySection = 'evidenceBase';
+
 			
 			$scope.renderInVitroLevel = function(inVitroResult) {
 				if(inVitroResult == null) {
@@ -38,4 +41,68 @@ hcvApp.controller('hcvRapCtrl',
 			})
 			.error(glueWS.raiseErrorDialog(dialogs, "rendering resistance associated polymorphism"));
 
+			$scope.freqNoteWhereClause = "variation.phdr_ras.id = '"+$scope.rapId+"'";
+
+			$scope.updateFreqNotePage = function(pContext) {
+				console.log("updateFreqNotePage", pContext);
+				
+				var cmdParams = {
+						"tableName": "var_almt_note",
+			            "whereClause": $scope.freqNoteWhereClause,
+			            "allObjects": false,
+			            "rendererModuleName": "phdrRasFrequencyRenderer"
+				};
+				pContext.extendListCmdParams(cmdParams);
+				glueWS.runGlueCommand("", {
+				    "multi-render": cmdParams
+				})
+			    .success(function(data, status, headers, config) {
+			    	$scope.rasNoteList = data.multiRenderResult.resultDocument;
+					console.info('listing RAP frequency notes result as object list', $scope.rasNoteList);
+			    })
+			    .error(glueWS.raiseErrorDialog(dialogs, "listing RAP frequency notes"));
+			}
+			
+			$scope.updateFreqNoteCount = function(pContext) {
+				console.log("updateFreqNoteCount", pContext);
+				
+				var cmdParams = {
+			            "whereClause": $scope.freqNoteWhereClause
+				};
+				pContext.extendCountCmdParams(cmdParams);
+				glueWS.runGlueCommand("", {
+			    	"count": { 
+				        "var-almt-note":cmdParams
+			    	} 
+				})
+			    .success(function(data, status, headers, config) {
+					console.info('count RAS frequency notes raw result', data);
+					$scope.freqNotePagingContext.setTotalItems(data.countResult.count);
+					$scope.freqNotePagingContext.firstPage();
+			    })
+			    .error(glueWS.raiseErrorDialog(dialogs, "counting RAP frequency notes"));
+			}
+			
+			$scope.freqNotePagingContext = pagingContext.createPagingContext($scope.updateFreqNoteCount, $scope.updateFreqNotePage);
+
+			$scope.freqNotePagingContext.setDefaultSortOrder([
+  	            { property:"ncbi_curated_frequency", displayName: "Frequency", order: "-"   }
+			]);
+
+			$scope.freqNotePagingContext.setSortableProperties([
+	            { property:"ncbi_curated_frequency", displayName: "Frequency" }
+            ]);
+
+			
+			$scope.freqNotePagingContext.setFilterProperties([
+	            { property: "alignment.displayName", altProperties:["alignment.name"], displayName: "Genotype / subtype", filterHints: {type: "String"} },
+	    		{ property: "ncbi_curated_frequency", displayName: "Frequency (percentage)", filterHints: {type: "Double"} }
+			]);
+			                          			
+			$scope.freqNotePagingContext.setDefaultFilterElems([]);
+
+			
+			$scope.freqNotePagingContext.countChanged();
+
+			
 		}]);
