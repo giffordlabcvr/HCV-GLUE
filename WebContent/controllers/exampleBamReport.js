c5164ee0e89bfd15343fee3d65ffd58d14fb5739
Josh Singer <josh.singer@glasgow.ac.uk>
2018-10-04 12:27:25 +0100

    removed unused js / html

--- WebContent/controllers/exampleBamReport.js
+++
@@ -1,6 +0,0 @@
-hcvApp.controller('exampleBamReportCtrl', 
-		[ '$scope', 'glueWS', 'dialogs', 'saveFile', 'FileSaver', '$http',
-		function($scope, glueWS, dialogs, saveFile, FileSaver, $http) {
-
-			
-		} ]);
--- WebContent/controllers/exampleFastaReport.js
+++
@@ -1,6 +0,0 @@
-hcvApp.controller('exampleFastaReportCtrl', 
-		[ '$scope', 'glueWS', 'dialogs', 'saveFile', 'FileSaver', '$http',
-		function($scope, glueWS, dialogs, saveFile, FileSaver, $http) {
-
-			
-		} ]);
